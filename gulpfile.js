6e9a5d181c855ee08212534d7401fa3236e59851
A.J. Brisson <brissona@gmail.com>
2019-12-17 16:29:21 -0500

    Adds gulp to project with initial sass and js files for testing gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,37 @@
 /* Gulp should compile sass and make client side JS compatible - feel free to add your own tasks! */
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+const nodemon = require('gulp-nodemon');
+
+sass.compiler = require('node-sass');
+
+function css () {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+};
+
+function javascript () {
+  return gulp.src('./js/**/*.js')
+    .pipe(babel({
+      presets: ['@babel/preset-env']
+    }))
+    .pipe(gulp.dest('./public/js'));
+};
+
+gulp.task('css', css);
+gulp.task('javascript', javascript);
+
+exports.default = function(done) {
+  nodemon({
+    script: './bin/www',
+    ext: 'js css',
+    done: done
+  });
+  // You can use a single task
+  gulp.watch('scss/**/*.scss', css);
+  // Or a composed task
+  gulp.watch('js/**/*.js', javascript);
+};
