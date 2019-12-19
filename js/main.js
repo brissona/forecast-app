5262f4f0243c706297fbe1805e17f592cd73470e
A.J. Brisson <brissona@gmail.com>
2019-12-19 09:56:23 -0500

    Adds forecast request form. Includes api request for testing. Adds axios as dependency.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
-console.log('hello');
-console.log('hello world');
+// Frontend validation.
+const forecastForm = () => {
+  const form = document.forms.forecast;
+
+  form.addEventListener('submit', (e) => {
+    if (!form.checkValidity()) e.preventDefault();
+  });
+};
+
+window.addEventListener('load', () => {
+  // Include form related js if form is in current view.
+  if (document.forms.forecast) forecastForm();
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
+
+function getWeather() {
+  return axios.get('https://api.darksky.net/forecast/a55b319b60b5035490f1e8370afbed97/37.8267,-122.4233')
+    .then(function (response) {
+      // handle success
+      return response.data.currently.summary;
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Express'
+  });
+});
+
+router.post('/', function(req, res) {
+  getWeather()
+    .then(function(sum) {
+      res.render('forecast', {
+        title: 'A post appeared!' + req.body.zipcode + ' ' + sum
+      });
+    })
 });
 
 module.exports = router;
